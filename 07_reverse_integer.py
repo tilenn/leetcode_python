72c592a75589198d1abee58c8d72f00397c773bc
tilenn <to8803@student.uni-lj.si>
2022-02-10 00:54:11 +0100

    07 faster

--- 07_reverse_integer.py
+++ 07_reverse_integer.py
@@ -1,13 +1,22 @@
+import math
+
+
 def reverse(x):
+    MIN = int(-(2**31))  # -2^31,
+    MAX = int(2**31 - 1)  #  2^31 - 1
+
     res = 0
-    tmp = x if x > 0 else -x
-    while tmp != 0:
-        res *= 10
-        res += tmp % 10
-        tmp = tmp // 10
-    if res < -(2**31) or res > 2**31 + 1:
-        return 0
-    return res if x > 0 else -res
+    while x:
+        zadnja_cifra = int(math.fmod(x, 10))
+        x = int(x / 10)
+
+        if res > MAX // 10 or (res == MAX // 10 and zadnja_cifra >= MAX % 10):
+            return 0
+        if res < MIN // 10 or (res == MIN // 10 and zadnja_cifra <= MIN % 10):
+            return 0
+        res = (res * 10) + zadnja_cifra
+
+    return res
 
 
 print(reverse(123))
