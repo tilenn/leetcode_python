cbf45324da423ceedfb13eca6023ad7d9378d4b5
tilenn <to8803@student.uni-lj.si>
2022-02-10 00:14:53 +0100

    06 faster

--- 06_zigzag.py
+++ 06_zigzag.py
@@ -1,37 +1,17 @@
 def convert(s, numRows):
-    if len(s) <= numRows or numRows == 1:
+    if numRows == 1:
         return s
-    step = 2 * numRows - 2
-    formatted_str = ""
-    idx = step
-    for i in range(numRows):
-        for j in range(len(s) // numRows + 1):
-            print(i + j * step)
-            if i + j * step < len(s):
-                formatted_str += s[i + j * step]
+    res = ""
+    step = 2 * (numRows - 1)
 
-            if i != 0 and i != numRows - 1:
-                # print(idx + j * step)
-                if idx + j * step < len(s):
-                    formatted_str += s[idx + j * step]
-        idx -= 1
-    # print(formatted_str)
-    return formatted_str
+    for row in range(numRows):
+        for i in range(row, len(s), step):
+            # print(s[i])
+            res += s[i]
+            # ce nismo v prvi ali zadnji
+            if 0 < row < numRows - 1 and i + step - 2 * row < len(s):
+                res += s[i + step - 2 * row]
+    return res
 
 
-# def printVmes(s, numRows):
-#     step = 2 * numRows - 2
-#     formatted_str = ""
-#     idx = step - 1
-#     for j in range(numRows):
-#         if j != 0 and j != numRows - 1:
-#             for i in range(numRows):
-#                 if idx + i * step < len(s):
-#                     formatted_str += s[idx + i * step]
-#             idx -= 1
-#     print(formatted_str)
-
-
-print(convert("PAYPALISHIRING", 2))
-# printVmes("PAYPALISHIRING", 3)
-# printVmes("a", 3)
+print(convert("PAYPALISHIRING", 4))
