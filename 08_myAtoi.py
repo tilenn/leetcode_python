d664c9d1ac0a3a749c3811864494c72ba7395219
tilenn <to8803@student.uni-lj.si>
2022-02-10 01:38:40 +0100

    08 a bit faster

--- 08_myAtoi.py
+++ 08_myAtoi.py
@@ -3,47 +3,46 @@ def myAtoi(s):
     MAX = 2**31 - 1
 
     res = 0
+
+    # nam pove ali je plus ali minus
     sign = False
+    # nam pove ali smo + ali minus ze videli
     sign_read = False
-    flag = False
+    # nam pove ali trenutno beremo stevilo
+    reading_num = False
+
     digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     for letter in s:
-        # print("letter:", letter)
-        if letter == " ":
-            if flag:
-                break
-            if res == 0:
-                continue
-            else:
-                break
 
+        # vidimo whitespace, ce smo do sedaj brali stevilo -> BREAK
+        # cene nadaljujemo
+        if letter == " ":
+            if reading_num or sign_read:
+                break
+            continue
+
+        # vidimo minus
+        # ce smo ga ze prebrali, ali smo do sedaj brali stevilo -> BREAK
         if letter == "-":
-            if flag:
+            if reading_num or sign_read:
                 break
-            flag = True
-            if sign_read:
-                break
-            else:
-                sign = True
-                sign_read = True
-                continue
+            sign = True
+            sign_read = True
+            continue
 
         if letter == "+":
-            if flag:
+            if reading_num or sign_read:
                 break
-            flag = True
-            if sign_read:
-                break
-            else:
-                sign_read = True
-                continue
+            sign_read = True
+            continue
 
         if letter not in digits:
             break
 
         if letter in digits:
-            flag = True
+            reading_num = True
             res = (res * 10) + int(letter)
+
     res = -res if sign else res
 
     if res > MAX:
