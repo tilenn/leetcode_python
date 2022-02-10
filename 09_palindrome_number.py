0299d0d6e6266932e08bd63ef285d7168f39eedb
tilenn <to8803@student.uni-lj.si>
2022-02-10 02:01:09 +0100

    09 v_2 isto

--- 09_palindrome_number.py
+++ 09_palindrome_number.py
@@ -11,6 +11,27 @@ def isPalindrome(x):
     return res == x
 
 
-print(isPalindrome(121))
-print(isPalindrome(-121))
-print(isPalindrome(11311))
+def isPalindrome_v2(x):
+    if x < 0:
+        return False
+
+    div = 1
+    while x >= 10 * div:
+        div *= 10
+
+    while x:
+        r = x % 10
+        l = x // div
+
+        if l != r:
+            return False
+
+        x = (x % div) // 10
+        div = div // 100
+
+    return True
+
+
+print(isPalindrome_v2(121))
+print(isPalindrome_v2(-121))
+print(isPalindrome_v2(11311))
